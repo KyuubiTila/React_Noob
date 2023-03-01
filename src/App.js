c01e6649a5704c29d02e21f075b1eeafbbb7f383
KyuubiTila <tylerkatsumot@gmail.com>
2023-03-01 18:22:35 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import { Header } from './components/Header'; // importing the module Header from the directory
+import { Footer } from './components/Footer';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [tasks, setTasks] = useState([
+    { id: 1534, name: 'Record react Lectures', completed: true },
+    { id: 2234, name: 'Record joint Lectures', completed: false },
+    { id: 4653, name: 'Record happy Lectures', completed: false },
+  ]);
+
+  const [show, setShow] = useState(true); // setting another use state for the toggler button
+
+  const handleDelete = (id) => {
+    setTasks(tasks.filter((task) => id !== task.id));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <h1>Task List</h1>
+      {/* manipulating the toggler to change the set state property whenever clicked */}
+      <button onClick={() => setShow(!show)}>Toggle</button>
+      <ul>
+        {/* the show useState acting as a conditioner preceeding the tasks useState
+        ie if the show state is false the preceeded should not happen */}
+        {show &&
+          tasks.map((task) => (
+            <li
+              // always setting the key for the map
+              key={task.id}
+              // using the ternary to add css functionality to the className attribute
+              className={task.completed ? 'completed' : 'incomplete'}
+            >
+              <span>
+                {task.id} - {task.name}
+                <button
+                  onClick={() => handleDelete(task.id)}
+                  className="delete"
+                >
+                  delete
+                </button>
+              </span>
+            </li>
+          ))}
+      </ul>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+import Logo from '../assets/logo.png'; // the double dot is to move out of the current directory which is components folder since the assets folder is also a directory inside of the src folder so as to be able to dive into the intented folder
+export const Header = () => {
+  return (
+    <div>
+      <img className="logo" src={Logo} alt="" />
+    </div>
+  );
+};
